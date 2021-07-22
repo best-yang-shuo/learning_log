5bc39f01a0884274bcef5b97acf4013b931211e1
best-yang-shuo <1425274732@qq.com>
2021-07-22 16:35:06 +0800

    添加404和500页面

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render,redirect
 from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import Http404
 # Create your views here.
@@ -18,7 +18,7 @@ def topics(request):
 @login_required()
 def topic(request, topic_id):
     """显示单个主题及其所有项目"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic,id=topic_id)
     #确认请求的主题属于当前用户
     if topic.owner != request.user:
         raise Http404
